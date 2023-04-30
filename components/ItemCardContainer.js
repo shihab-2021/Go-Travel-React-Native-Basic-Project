03c45cc2c6abee64a699be97bddd3a2076fd1a7a
Shajibul Alam Shihab <shihab@Shihab.Shajibul>
2023-04-30 10:01:48 +0600

    update

--- components/ItemCardContainer.js
+++ components/ItemCardContainer.js
@@ -4,9 +4,12 @@ import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 const ItemCardContainer = ({ imageSrc, title, location, data }) => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("ItemScreen", { param: data})} className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px] my-2 ">
+    <TouchableOpacity
+      onPress={() => navigation.navigate("ItemScreen", { param: data })}
+      className="rounded-md border border-gray-300 space-y-2 px-3 py-2 shadow-md bg-white w-[182px] my-2 "
+    >
       <Image
         source={{ uri: imageSrc }}
         className="w-full h-40 rounded-md object-cover "
@@ -17,9 +20,9 @@ const ItemCardContainer = ({ imageSrc, title, location, data }) => {
       <View className="flex-row items-center space-x-1 ">
         <FontAwesome name="map-marker" size={20} color={"#8597A2"} />
         <Text className="text-[#428288] text-[14px] font-bold ">
-            {location?.length > 18 ? `${title?.slice(0, 18)}..` : location}
+          {location?.length > 18 ? `${location?.slice(0, 18)}..` : location}
         </Text>
-      </View> 
+      </View>
     </TouchableOpacity>
   );
 };
